7101728d481064c1e54ad69f7bb2571539b30a5f
AidenLeeeee <hoo8799@naver.com>
2022-04-25 15:45:57 +0900

    Created Prototype

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, jsonify
-import io
-import torchvision.transforms as transforms
-from PIL import Image
+from flask import Flask, jsonify, request, render_template, redirect
+import os
+from inference import get_prediction
+from commons import format_class_name
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        class_name = format_class_name(class_name)
+        return render_template('result.html', class_id=class_id, class_name=class_name)
+
+    return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    return jsonify({'class_id': 'IMAGE_NET_XXX', 'class_name': 'Cat'})
+    if request.method == 'POST':
+        # we will get the file from the request
+        file = request.files['file']
+        # convert that to bytes
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_id': class_id, 'class_name': class_name})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
---
+++ commons.py
@@ -0,0 +1,26 @@
+from torchvision import models, transforms
+from PIL import Image
+import io
+
+
+def get_model():
+    model = models.densenet121(pretrained=True)
+    model.eval()
+    return model
+
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+def format_class_name(class_name):
+    class_name = class_name.replace('_', ' ')
+    class_name = class_name.title()
+    return class_name
